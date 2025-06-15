3088a686e3a12b8fcff50b41159af11b75d8ed6c
afifudin-f <afif.abdullah@feedloop.ai>
2025-06-15 17:49:51 +0700

    fix breakpoint

--- app/notes/page.jsx
+++ app/notes/page.jsx
@@ -11,7 +11,7 @@ export default async function Page() {
 
   return (
     <main>
-      <div className="max-w-4xl mx-auto p-4 sm:p-8">
+      <div className="max-w-4xl mx-auto p-8 md:p-4">
         <p className="text-xs text-neutral-500">Logged as {username}</p>
         <div className="flex flex-col items-center py-24 gap-4">
           <div className="text-center">
@@ -24,7 +24,7 @@ export default async function Page() {
           </div>
           <NewNoteDialog />
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+        <div className="grid grid-cols-1 max-w-xs mx-auto sm:max-w-full sm:grid-cols-2 md:grid-cols-3 gap-12">
           {notes.map((note) => {
             return (
               <NoteCard
